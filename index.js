119d06cef57544b66e2535630e3965831091804b
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2022-05-06 17:36:26 -0300

    add img, text and index.js

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-const frase = "Right now it's 10 o'clock"
-const a = document.createTextNode(frase);
+// var img = document.querySelector("#image");
+// var date = new Date();
+// var hour = date.getHours();
+// var minute = date.getMinutes();
 
-var time = document.querySelector("div");
-time.appendChild(a);
+// function load(hour) {
+//     if (hour >= 0 && hour < 12) {
+//         img.src = "./img/morning.jpg";
+//         document.body.style.backgroundColor = "#bbe8b5";
+//     } else if (hour >= 12 && hour < 19) {
+//         img.src = "./img/afternoon.jpg";
+//         document.body.style.backgroundColor = "#cab076";
+//     } else if (hour >= 19 && hour < 23 ) {
+//         img.src = "./img/evening.jpg";
+//         document.body.style.backgroundColor = "#112d3d";
+//     }
+
+//     const phrase = `Right now it's ${hour}:${minute}`
+//     const a = document.createTextNode(phrase);
+//     var timeDiv = document.querySelector(".time");
+//     timeDiv.appendChild(a);
+// }
+
+var img = document.querySelector("#image");
+var date = new Date();
+var hour = date.getHours();
+var minute = date.getMinutes();
+
+if (hour >= 0 && hour < 12) {
+  img.src = "./img/morning.jpg";
+  document.body.style.backgroundColor = "#bbe8b5";
+} else if (hour >= 12 && hour < 19) {
+  img.src = "./img/afternoon.jpg";
+  document.body.style.backgroundColor = "#cab076";
+} else if (hour >= 19 && hour < 23) {
+  img.src = "./img/evening.jpg";
+  document.body.style.backgroundColor = "#112d3d";
+}
+
+const phrase = `Right now it's ${hour}:${minute}`;
+const a = document.createTextNode(phrase);
+var timeDiv = document.querySelector(".time");
+timeDiv.appendChild(a);
